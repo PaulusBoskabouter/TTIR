841c02be96c4e8d7601056e123b3d88c4a7a6cfa
Paul V <acc.vvn@pm.me>
2025-11-06 13:03:16 +0100

    0 shame using github to transfer the latest files from laptop to pc (too lazy to branch)

--- utils/user_featuers.py
+++ utils/user_featuers.py
@@ -1,7 +1,17 @@
+"""
+# Author       : Paul Verhoeven
+# Date         : 03-11-2025
+
+To keep the notebook as uncluttered as possible, I've thrown a lot of functions down here below.
+"""
+
 import pandas as pd 
 
-def fetch_user_likes(timestamp:int, user:int, likes:pd.DataFrame, unlikes:pd.DataFrame, dislikes:pd.DataFrame, undislikes:pd.DataFrame) -> dict:
+
+def song_lables_and_interactions(timestamp:int, user:int, likes:pd.DataFrame, unlikes:pd.DataFrame, dislikes:pd.DataFrame, undislikes:pd.DataFrame, get_as_label:bool = True) -> dict:
     like_tracker = {}
+
+    interactions = 0
     
     # Subset our dataframes to contain only user id and timestramp up until the given timestamp parameter.
     likes = likes[(likes['uid'] == user) & (likes['timestamp'] <= timestamp)]
@@ -21,6 +31,7 @@ def fetch_user_likes(timestamp:int, user:int, likes:pd.DataFrame, unlikes:pd.Dat
             like_tracker[song] += 1
         except KeyError:
             like_tracker[song] = 1
+        interactions += 1
     
     # For dislikes
     for row in range(len(dislikes)):
@@ -29,6 +40,7 @@ def fetch_user_likes(timestamp:int, user:int, likes:pd.DataFrame, unlikes:pd.Dat
             like_tracker[song] -= 1
         except KeyError:
             like_tracker[song] = -1
+        interactions += 1
     
     
 
@@ -37,11 +49,29 @@ def fetch_user_likes(timestamp:int, user:int, likes:pd.DataFrame, unlikes:pd.Dat
     for row in range(len(unlikes)):
         song = unlikes.iloc[row]['item_id']
         like_tracker[song] -= 1
+        interactions -= 1
     
     # For undislikes
     for row in range(len(undislikes)):
         song = undislikes.iloc[row]['item_id']
         like_tracker[song] += 1
+        interactions -= 1
 
+    
+    if get_as_label:
+        for item, score in like_tracker.items():
+            if score > 0:
+                like_tracker[item] = 1
+            if score < 0:
+                like_tracker[item] = -1
             
-    return like_tracker
+    return like_tracker, interactions
+
+
+
+
+def get_user_features():
+    ...
+
+
+
