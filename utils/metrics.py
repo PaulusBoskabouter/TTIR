5a8d6bff0d5a69b0a94d19ecdfacfe1386fb6614
Beau Nienhuis <beau.nienhuis@student.ru.nl>
2025-12-10 17:12:31 +0100

    change metrics for non-binary & continuous labels

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,93 +1,142 @@
-#import torch
+# IMPORTS
+
 import numpy as np
-import math as log2
 
 
 
-def compute_precision_at_k(ground_truth_sets, ranked_item_list, k):
-    " compute precision at k per user and average over users"
-    precisions = []
-    for relevant, recommended in zip(ground_truth_sets, ranked_item_list):
-        top_k = recommended[:k]
-        correct_hit = len(set(top_k) & relevant)
-        precisions.append(correct_hit / k if k > 0 else 0.0)
-    return float(np.mean(precisions))
+# HELPERS
+
+"""
+Rescales values to range [0, 1]
+    Expects np array
+    Mainly relevant for like/dislikes (unbounded negative and positive values)
+    For range [-1, 1], will map 0 to 0.5
+"""
+def rescale(array):
+    min_, max_ = min(array), max(array)
+    return (array - min_) / (max_ - min_)
+
+"""
+Converts values to 0 or 1
+    -1 is mapped to 0, 
+    1+ is mapped to 1
+    0-1 (real numbers) are mapped according to a threshold
+"""
+def remap(array, t = 0.8):
+    return np.array([1 if score > t else 0 for score in array])
+
+"""
+Calculates all metrics for a given ranking & k (& t).
+Expects np.array for ranking
+"""
+def metrics(ranking, K, t = 0.8):
+
+    # Calculate metrics for different k
+    P       = [precision(ranking, k, t) for k in K]
+    R       = [recall(ranking, k, t)    for k in K]
+    F1      = [f1_score(ranking, k, t)  for k in K]
+    N_K     = [NDCG(ranking, k)         for k in K]
+    N       = [NDCG(ranking)]
+    metrics = np.array(P + R + F1 + N_K + N)
+
+    return metrics
+
+"""
+Aggregate metrics for a set of k. Expects 2D list of metrics
+"""
+def aggregate(metrics, K):
+    
+    # Calculate metrics for different k
+    P       = [f"Precision @ {k}"   for k in K]
+    R       = [f"Recall @ {k}"      for k in K]
+    F1      = [f"F1-score @ {k}"    for k in K]
+    N_K     = [f"NDCG @ {k}"        for k in K]
+    N       = ["NDCG"]
+    labels = np.array(P + R + F1 + N_K + N)
+
+    # Aggregate metrics
+    aggregate = np.mean(metrics, axis = 0)
+
+    # Save & return in dictionary
+    return {str(key): float(value) for key, value in zip(labels, aggregate)}
 
 
-def recall_at_k(ground_truth_sets, ranked_item_list, k):
-    " compute recall at k per user and average over users"
-    recalls = []
-    for relevant, recommended in zip(ground_truth_sets, ranked_item_list):
-        # no relevant items, skip
-        if len(relevant) == 0:
-            recalls.append(0.0)
-            continue 
-        top_k = recommended[:k]
-        correct_hit = len(set(top_k) & relevant)
-        recalls.append(correct_hit / len(relevant))
-    return float(np.mean(recalls))
+
+# METRICS (BINARY)
+
+"""
+Calculate precision score. Note: Precision is defined for binary labels
+"""
+def precision(ranking, k, t = 0.8):
+
+    # Convert non-binary values
+    labels = remap(ranking, t)
+
+    # Calculate precision
+    return sum(labels[:k]) / k if k > 0 else 0
+
+"""
+Calculate recall score. Note: Recall is defined for binary labels
+"""
+def recall(ranking, k, t = 0.8):
+
+    # Convert non-binary values
+    labels = remap(ranking, t)
+
+    # Calculate precision
+    return sum(labels[:k]) / sum(labels) if sum(labels) > 0 else 0
+
+"""
+Calculate F1 score. Note: F1 is defined for binary labels
+"""
+def f1_score(ranking, k, t = 0.8):
+
+    # Calculate precision & recall
+    p, r = precision(ranking, k, t), recall(ranking, k, t)
+
+    # Calculate F1
+    return (2 * p * r) / (p + r) if p != 0 and r != 0 else 0
 
 
-def f1_score_at_k(ground_truth_sets, ranked_item_list, k):
-    " compute F1 score at k per user and average over users"
-    f1_scores= []
-    for relevant, recommended in zip(ground_truth_sets, ranked_item_list):
-        top_k = recommended[:k]
-        correct_hit = len(set(top_k) & relevant)
-        # no relevant items, skip
-        if len(relevant) == 0 or k == 0:
-            f1_scores.append(0.0)
-            continue 
-        precision = correct_hit / k
-        recall = correct_hit / len(relevant) 
-        if precision + recall == 0:
-            continue
-        else:
-            f1_scores.append(2 * precision * recall / (precision + recall))
+"""
+Calculate AP score. Note: AP is defined for binary labels
+"""
+def average_precision(ranking, t = 0.8):
 
-    return float(np.mean(f1_scores))
+    # Calculate average precision
+    return sum([ranking[k] * precision(ranking, k + 1, t) for k in range(len(ranking))]) / sum(ranking) if sum(ranking) > 0 else 0
 
 
-def average_precision(relevant, recommended, k):
-    " compute average precision at k for a single user "
-    correct_hit= 0
-    scores = []
-    if len(recommended) > k:
-        recommended = recommended[:k]
-    for i, song in enumerate(recommended, start=1):
-        if song in relevant:
-            correct_hit += 1
-            scores.append(correct_hit / i)
-    if len(relevant) == 0 or len(scores) == 0:
-        return 0.0
-    return sum(scores) / len(relevant)
+
+# LABELS (CONTINUOUS/NON-BINARY)
+
+"""
+Calculate DCG score. DCG works for non-binary labels
+"""
+def DCG(ranking, k = None):
+
+    # Rescale labels to [0, 1]
+    labels = rescale(ranking)
+
+    if k is not None:
+
+        # Calculate DCG
+        return sum([(2 ** labels[i] - 1) / np.log2(2 + i) for i in range(k)])
+    
+    else:
+
+        # Calculate DCG
+        return sum([(2 ** labels[i] - 1) / np.log2(2 + i) for i in range(len(labels))])
 
 
-def mean_average_precision(ground_truth_sets, ranked_item_list, k):
-    " compute mean average precision at k over all users "
-    MAP_scores = []
-    for relevant, recommended in zip(ground_truth_sets, ranked_item_list):
-        ap_score_per_user = average_precision(relevant, recommended, k)
-        MAP_scores.append(ap_score_per_user)
-    return float(np.mean(MAP_scores))
 
+"""
+Calculate DCG score. DCG works for non-binary labels
+"""
+def NDCG(ranking, k = None):
 
-def ndcg_at_k(ground_truth_sets, ranked_item_list, k):
-    " compute normalized discounted cumulative gain at k per user and average over users"
-    ndcgs = []
-    for relevant, recommended in zip(ground_truth_sets, ranked_item_list):
-        # discounted cumulative gain
-        dcg = 0.0
-        for i, item in enumerate(recommended[:k], start=1):
-            if item in relevant:
-                dcg += 1.0 / log2(i + 1)
-
-        ideal_hits = min(len(relevant), k)
-        # ideal discounted cumulative gain
-        if ideal_hits == 0:
-            ndcgs.append(0.0)
-            continue
-        idcg = sum(1.0 / log2(i + 1) for i in range(1, ideal_hits + 1))
-        # normalized discounted cumulative gain
-        ndcgs.append(dcg / idcg)
-    return float(np.mean(ndcgs))
+    # Sort in descending order
+    ideal = np.sort(ranking)[::-1]
+    
+    # Calculate NDCG
+    return DCG(ranking, k) / DCG(ideal, k) if sum(ranking) != 0 else 0
